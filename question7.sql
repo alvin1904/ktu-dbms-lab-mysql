05611efcbec234cb8ab45bda3602b72f3375cede
alvin1904 <alvin19official@gmail.com>
2022-12-05 15:28:30 +0530

    add data of q7

---
+++ question7.sql
@@ -0,0 +1,148 @@
+create database d7;
+use d7;
+
+
+create table StudentDetails (
+	name VARCHAR(50),
+	rollno INT,
+	utyregno VARCHAR(9),
+	address VARCHAR(50),
+	year_of_admin INT,
+	year_of_passout INT,
+	branch VARCHAR(3),
+	class_teacher VARCHAR(10),
+	emailid VARCHAR(50),
+	phoneno VARCHAR(10)
+);
+
+insert into StudentDetails values ('Reade', 1, 'KTE59XX70', '72025 Sherman Parkway', 2020, 2024, 'EEE', 'Aswin', 'rblumer0@mapy.cz', '7044958412');
+insert into StudentDetails values ('Gracia', 2, 'KTE96XX57', '9723 Buhler Crossing', 2020, 2024, 'CSE', 'Amalia', 'gcrufts1@mlb.com', '6059232571');
+insert into StudentDetails values ('Larine', 3, 'KTE99XX66', '57 Anderson Park', 2017, 2021, 'MEC', 'Arya', 'lduffill2@histats.com', '9301870105');
+insert into StudentDetails values ('Cyrill', 4, 'KTE05XX60', '8 Forest Circle', 2017, 2021, 'CIV', 'Hrishikesh', 'cdemaid3@imageshack.us', '6098931285');
+insert into StudentDetails values ('Alberto', 5, 'KTE84XX84', '65863 Stephen Pass', 2017, 2021, 'MEC', 'Arya', 'amacmychem4@domainmarket.com', '7478791342');
+insert into StudentDetails values ('Rowland', 6, 'KTE78XX78', '1954 Lerdahl Junction', 2017, 2021, 'CSE', 'Shaju', 'rkimmitt5@google.co.jp', '6436096352');
+insert into StudentDetails values ('Querida', 7, 'KTE97XX42', '75 Mayer Hill', 2017, 2021, 'CSE', 'Shaju', 'qboyles6@dedecms.com', '7259647900');
+insert into StudentDetails values ('Cortney', 8, 'KTE33XX46', '72058 Farragut Junction', 2017, 2021, 'EEE', 'Abru', 'ckelbie7@simplemachines.org', '8767658796');
+insert into StudentDetails values ('Wendell', 9, 'KTE08XX56', '5452 Clarendon Lane', 2018, 2022, 'ECE', 'Aswin', 'wsandever8@studiopress.com', '8376102289');
+insert into StudentDetails values ('Ruggiero', 10, 'KTE08XX40', '2050 Quincy Park', 2018, 2022, 'ECE', 'Aswin', 'rdionisio9@a8.net', '9047797177');
+insert into StudentDetails values ('Othelia', 11, 'KTE22XX47', '94542 Russell Court', 2019, 2023, 'MEC', 'Hrishikesh', 'olidyarda@guardian.co.uk', '8771090635');
+insert into StudentDetails values ('Evanne', 12, 'KTE32XX50', '7 Reindahl Avenue', 2017, 2021, 'CIV', 'Hrishikesh', 'echarnickb@cdbaby.com', '8917709000');
+insert into StudentDetails values ('Werner', 13, 'KTE67XX12', '477 Banding Point', 2017, 2021, 'MEC', 'Arya', 'wfargiec@uol.com.br', '6222432090');
+insert into StudentDetails values ('Rickie', 14, 'KTE94XX98', '4974 Clarendon Court', 2018, 2022, 'CSE', 'Amalia', 'ralessandruccid@bluehost.com', '6431856826');
+insert into StudentDetails values ('Joell', 15, 'KTE84XX57', '609 Lawn Drive', 2020, 2024, 'EEE', 'Aswin', 'jrobroee@adobe.com', '6280780207');
+insert into StudentDetails values ('Percival', 16, 'KTE52XX11', '6 Vahlen Hill', 2018, 2022, 'ECE', 'Aswin', 'pcruttendenf@cmu.edu', '9673248954');
+insert into StudentDetails values ('Kara-lynn', 17, 'KTE63XX38', '8 Delaware Park', 2018, 2022, 'CSE', 'Amalia', 'kquiltyg@amazon.com', '8107687778');
+insert into StudentDetails values ('Matty', 18, 'KTE30XX07', '173 Cherokee Court', 2020, 2024, 'CIV', 'Arya', 'mmeiningerh@51.la', '7379857870');
+insert into StudentDetails values ('Cristi', 19, 'KTE99XX47', '582 Reinke Pass', 2018, 2022, 'CSE', 'Amalia', 'cscholeyi@blinklist.com', '9571826511');
+insert into StudentDetails values ('Orel', 20, 'KTE64XX48', '54 Heath Center', 2020, 2024, 'CIV', 'Arya', 'osharpj@over-blog.com', '9903333410');
+insert into StudentDetails values ('Aidan', 21, 'KTE05XX69', '04 Esch Pass', 2020, 2024, 'ECE', 'Abru', 'achippsk@msn.com', '9175802447');
+insert into StudentDetails values ('Josefina', 22, 'KTE47XX24', '91 Elka Terrace', 2017, 2021, 'CSE', 'Shaju', 'jeakleel@earthlink.net', '7648113471');
+insert into StudentDetails values ('Em', 23, 'KTE63XX04', '0 Barnett Way', 2020, 2024, 'CIV', 'Arya', 'ephilippartm@jimdo.com', '7914861741');
+insert into StudentDetails values ('Wilton', 24, 'KTE88XX24', '3385 Elgar Avenue', 2018, 2022, 'MEC', 'Arya', 'wruffeyn@livejournal.com', '6932048542');
+insert into StudentDetails values ('Neils', 25, 'KTE36XX34', '63 Lakewood Gardens Avenue', 2017, 2021, 'EEE', 'Abru', 'ngeoghegano@twitpic.com', '7678293322');
+insert into StudentDetails values ('Laurent', 26, 'KTE12XX64', '681 Vermont Pass', 2017, 2021, 'CIV', 'Hrishikesh', 'litscowiczp@naver.com', '8174789731');
+insert into StudentDetails values ('Jere', 27, 'KTE07XX60', '60 Bluejay Lane', 2019, 2023, 'MEC', 'Hrishikesh', 'jmaroq@stumbleupon.com', '8736315464');
+insert into StudentDetails values ('Skipp', 28, 'KTE47XX23', '66969 Gulseth Center', 2020, 2024, 'CSE', 'Amalia', 'swycliffr@typepad.com', '8265029235');
+insert into StudentDetails values ('Bibbye', 29, 'KTE34XX22', '0373 Vernon Point', 2018, 2022, 'CSE', 'Amalia', 'bharrods@opensource.org', '7935644223');
+insert into StudentDetails values ('Evy', 30, 'KTE16XX19', '72 Truax Pass', 2020, 2024, 'ECE', 'Abru', 'ewitchert@forbes.com', '8335724213');
+
+create table CourseQualification (
+	utyregno VARCHAR(9),
+	total INT,
+	grades VARCHAR(11)
+);
+insert into CourseQualification values ('KTE59XX70', 73, 'SecondClass');
+insert into CourseQualification values ('KTE96XX57', 77, 'SecondClass');
+insert into CourseQualification values ('KTE99XX66', 87, 'FirstClass');
+insert into CourseQualification values ('KTE05XX60', 95, 'Distinction');
+insert into CourseQualification values ('KTE84XX84', 83, 'FirstClass');
+insert into CourseQualification values ('KTE78XX78', 73, 'SecondClass');
+insert into CourseQualification values ('KTE97XX42', 84, 'FirstClass');
+insert into CourseQualification values ('KTE33XX46', 76, 'SecondClass');
+insert into CourseQualification values ('KTE08XX56', 79, 'SecondClass');
+insert into CourseQualification values ('KTE08XX40', 83, 'FirstClass');
+insert into CourseQualification values ('KTE22XX47', 84, 'FirstClass');
+insert into CourseQualification values ('KTE32XX50', 94, 'Distinction');
+insert into CourseQualification values ('KTE67XX12', 83, 'FirstClass');
+insert into CourseQualification values ('KTE94XX98', 96, 'Distinction');
+insert into CourseQualification values ('KTE84XX57', 99, 'Distinction');
+insert into CourseQualification values ('KTE52XX11', 100, 'Distinction');
+insert into CourseQualification values ('KTE63XX38', 89, 'FirstClass');
+insert into CourseQualification values ('KTE30XX07', 81, 'FirstClass');
+insert into CourseQualification values ('KTE99XX47', 95, 'Distinction');
+insert into CourseQualification values ('KTE64XX48', 91, 'Distinction');
+insert into CourseQualification values ('KTE05XX69', 97, 'Distinction');
+insert into CourseQualification values ('KTE47XX24', 87, 'FirstClass');
+insert into CourseQualification values ('KTE63XX04', 91, 'Distinction');
+insert into CourseQualification values ('KTE88XX24', 91, 'Distinction');
+insert into CourseQualification values ('KTE36XX34', 80, 'SecondClass');
+insert into CourseQualification values ('KTE12XX64', 81, 'FirstClass');
+insert into CourseQualification values ('KTE07XX60', 100, 'Distinction');
+insert into CourseQualification values ('KTE47XX23', 94, 'Distinction');
+insert into CourseQualification values ('KTE34XX22', 81, 'FirstClass');
+insert into CourseQualification values ('KTE16XX19', 94, 'Distinction');
+
+create table ProjectDetails (
+	utyregno VARCHAR(9),
+	title VARCHAR(50),
+	guide VARCHAR(10),
+	period_of_implementation DECIMAL(1),
+	corearea VARCHAR(3)
+);
+insert into ProjectDetails values ('KTE59XX70', 'edu.stanford.Daltfresh', 'Aswin', 2, 'ECE');
+insert into ProjectDetails values ('KTE96XX57', 'org.pbs.Toughjoyfax', 'Hrishikesh', 3, 'CIV');
+insert into ProjectDetails values ('KTE99XX66', 'com.chronoengine.Matsoft', 'Arya', 3, 'MEC');
+insert into ProjectDetails values ('KTE05XX60', 'org.npr.Kanlam', 'Amalia', 2, 'CSE');
+insert into ProjectDetails values ('KTE84XX84', 'gov.census.Flowdesk', 'Amalia', 1, 'CSE');
+insert into ProjectDetails values ('KTE78XX78', 'com.springer.Quo Lux', 'Shaju', 3, 'CSE');
+insert into ProjectDetails values ('KTE97XX42', 'com.deviantart.Kanlam', 'Arya', 2, 'CIV');
+insert into ProjectDetails values ('KTE33XX46', 'br.com.uol.Job', 'Aswin', 1, 'ECE');
+insert into ProjectDetails values ('KTE08XX56', 'com.msn.Regrant', 'Arya', 1, 'CIV');
+insert into ProjectDetails values ('KTE08XX40', 'com.friendfeed.Greenlam', 'Arya', 2, 'MEC');
+insert into ProjectDetails values ('KTE22XX47', 'jp.co.yahoo.Span', 'Amalia', 1, 'CSE');
+insert into ProjectDetails values ('KTE32XX50', 'com.printfriendly.Quo Lux', 'Hrishikesh', 3, 'MEC');
+insert into ProjectDetails values ('KTE67XX12', 'com.blogtalkradio.Tampflex', 'Arya', 3, 'CIV');
+insert into ProjectDetails values ('KTE94XX98', 'jp.co.yahoo.Daltfresh', 'Hrishikesh', 1, 'CIV');
+insert into ProjectDetails values ('KTE84XX57', 'edu.wisc.Namfix', 'Aswin', 3, 'ECE');
+insert into ProjectDetails values ('KTE52XX11', 'org.wikipedia.Tresom', 'Hrishikesh', 2, 'CIV');
+insert into ProjectDetails values ('KTE63XX38', 'jp.co.amazon.Latlux', 'Arya', 2, 'MEC');
+insert into ProjectDetails values ('KTE30XX07', 'com.forbes.Matsoft', 'Arya', 2, 'CIV');
+insert into ProjectDetails values ('KTE99XX47', 'com.deviantart.Fintone', 'Aswin', 1, 'EEE');
+insert into ProjectDetails values ('KTE64XX48', 'org.unicef.Kanlam', 'Abru', 3, 'EEE');
+insert into ProjectDetails values ('KTE05XX69', 'com.ucoz.Viva', 'Arya', 1, 'MEC');
+insert into ProjectDetails values ('KTE47XX24', 'com.ibm.Konklab', 'Abru', 3, 'EEE');
+insert into ProjectDetails values ('KTE63XX04', 'com.pcworld.Lotstring', 'Shaju', 1, 'CSE');
+insert into ProjectDetails values ('KTE88XX24', 'com.fastcompany.Andalax', 'Abru', 3, 'EEE');
+insert into ProjectDetails values ('KTE36XX34', 'com.techcrunch.Trippledex', 'Aswin', 3, 'EEE');
+insert into ProjectDetails values ('KTE12XX64', 'com.webmd.Rank', 'Arya', 1, 'MEC');
+insert into ProjectDetails values ('KTE07XX60', 'com.wikia.Otcom', 'Arya', 2, 'MEC');
+insert into ProjectDetails values ('KTE47XX23', 'com.hc360.Zaam-Dox', 'Aswin', 3, 'EEE');
+insert into ProjectDetails values ('KTE34XX22', 'es.google.Vagram', 'Aswin', 3, 'ECE');
+insert into ProjectDetails values ('KTE16XX19', 'com.blogspot.Span', 'Abru', 3, 'ECE');
+
+create table FacultyDetails (
+	fname VARCHAR(10),
+	fid VARCHAR(50),
+	email VARCHAR(50),
+	designation VARCHAR(9),
+	joiningdate DATE,
+	relievingdate DATE
+);
+insert into FacultyDetails values ('Abru', 'KTF52YY80', 'solenane0@example.com', 'Prof', '2022-05-01', '2022-10-22');
+insert into FacultyDetails values ('Arya', 'KTF45YY93', 'jliven1@posterous.com', 'Prof', '2021-07-24', '2022-07-13');
+insert into FacultyDetails values ('Hrishikesh', 'KTF85YY58', 'efeldmesser2@chicagotribune.com', 'Prof', '2020-09-03', null);
+insert into FacultyDetails values ('Aswin', 'KTF11YY19', 'ecantos3@walmart.com', 'Ad_Hoc', '2018-01-14', null);
+insert into FacultyDetails values ('Amalia', 'KTF37YY55', 'marling4@toplist.cz', 'Asst_Prof', '2021-08-30', '2022-09-23');
+insert into FacultyDetails values ('Shaju', 'KTF40YY60', 'clyle5@reddit.com', 'Asst_Prof', '2018-08-17', null);
+
+select * from StudentDetails;
+select * from CourseQualification;
+select * from ProjectDetails;
+select * from FacultyDetails;
+
+select name, rollno, address
+from StudentDetails
+order by branch, year_of_admin;
+
+
+drop database d7;
